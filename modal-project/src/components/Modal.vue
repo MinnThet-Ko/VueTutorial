a2c2f9be68facb0f8e657e86e563be1cc096c1cb
MinnThet-Ko <blltspd@gmail.com>
2024-10-31 09:50:25 +0700

    Commit on 31/10/2024 09:50 AM - Followed the Tutorial 5

---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">Modal content</div>
+    </div>
+</template>
+<style>
+    .backdrop {
+        top: 0%;
+        left: 0%;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+    }
+
+    .modal {
+        width: 400px;
+        margin: 100px auto;
+        padding: 20px;
+        border-radius: 10px;
+        background: wheat;
+    }
+</style>
